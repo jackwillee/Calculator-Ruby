a57bc39ba813601dbac6091a1171a5373f4d27d4
jack.willee <jackwillee1995@gmail.com>
2023-02-07 13:55:08 +0100

    addition method for calculator

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,8 +1,12 @@
 require 'calculator.rb'
 describe Calculator do 
+    describe 'Calculator class' do 
+    it 'calc.add(1,1) should return 2' do 
+        calc = Calculator.new
+        expect(calc.add(1,1)).to eq(2)
 
 
 
-
-
+    end 
+    end 
 end 
